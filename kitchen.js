8d52f14fd755373eb8879a1c6a97b3a2c84fd562
Ravi51234 <140091754+Ravi51234@users.noreply.github.com>
2024-06-17 14:31:09 +0530

    Add files via upload

---
+++ kitchen.js
@@ -0,0 +1,49 @@
+status = "";
+bedroom = ""
+objects = [];
+
+function back(){
+    window.location = "index.html";
+}
+
+function preload(){
+    bedroom = loadImage("kitchen.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(400, 400);
+    canvas.position(475, 180);
+
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    status = true;
+    objectDetector.detect(bedroom, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        objects = results;
+    }
+}
+
+function draw(){
+    image(bedroom, 0, 0, 400, 400);
+    if(status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("number").innerHTML = "There are 5 big objects in this image out of which COCOSSD model have guessed 3 objects";
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
---
+++ main.js
@@ -0,0 +1,19 @@
+function go_bedroom(){
+    window.location = "bedroom.html";
+}
+
+function go_dining_room(){
+    window.location = "dining room.html";
+}
+
+function go_drawing_room(){
+    window.location = "drawing room.html";
+}
+
+function go_kitchen(){
+    window.location = "kitchen.html";
+}
+
+function go_wardrobe(){
+    window.location = "wardrobe.html";
+}
